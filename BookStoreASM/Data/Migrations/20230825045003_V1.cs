6e0be292f34d47ea1852fef2519a8bd5fcd6c807
Đặng Phúc Minh <s2vethan2s@outlook.com>
2023-08-27 14:02:45 +0700

    Add project files.

---
+++ BookStoreASM/Data/Migrations/20230825045003_V1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStoreASM.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    DatePublished = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ BookStoreASM/Models/Book.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreASM.Models
+{
+    public class Book
+    {
+        public string? Id { get; set; }
+
+        [Required]
+
+        public string? Title { get; set; }
+
+        [Required, MaxLength(100)]
+
+        public string Description { get; set; }
+
+        public string Language { get; set; }
+
+        [Required, MaxLength(13)]
+
+        public string ISBN { get; set; }
+
+        [Required,
+         DataType(DataType.Date),
+         Display(Name = "Date Published")]
+
+        public string DatePublished { get; set; }
+
+        [Required, DataType(DataType.Currency)]
+
+        public int Price { get; set; }
+
+        [Required]
+
+        public string Author { get; set; }
+
+        [Display(Name = "Image URL")]
+
+        public string ImageUrl { get; set; }
+
+    }
+}
