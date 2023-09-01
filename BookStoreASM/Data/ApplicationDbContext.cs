0bbd5a884bec99ff6780f0c3b8359e35cd4be82e
Jeremy <0baogiohet39@gmail.com>
2023-09-01 21:37:16 +0700

    v1

--- BookStoreASM/Data/ApplicationDbContext.cs
+++ BookStoreASM/Data/ApplicationDbContext.cs
@@ -16,5 +16,9 @@ namespace BookStoreASM.Data
         public DbSet<CartItem> CartItems { get; set; }
 
         public DbSet<BookStoreASM.Models.ApplicationRole> ApplicationRole { get; set; } = default!;
+
+        public DbSet<Order> Order { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
